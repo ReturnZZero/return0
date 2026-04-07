121c1cf4109f725b72cda5bf0f3ecc99e3a1658c
Yena Yoo <yny9908@ewhain.net>
2026-04-07 13:22:14 +0900

    firestore api  모듈 추가

---
+++ 26-1 GROWTH/lib/common/api/firestore_service.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  FirestoreService({FirebaseFirestore? firestore})
+    : _firestore = firestore ?? FirebaseFirestore.instance;
+
+  final FirebaseFirestore _firestore;
+
+  static const String tourPlacesCollection = 'tour_places';
+  static const int _maxBatchSize = 400;
+
+  Future<int> upsertTourPlaces(List<Map<String, dynamic>> places) async {
+    if (places.isEmpty) {
+      return 0;
+    }
+
+    var savedCount = 0;
+
+    for (var i = 0; i < places.length; i += _maxBatchSize) {
+      final chunk = places.skip(i).take(_maxBatchSize).toList();
+      final batch = _firestore.batch();
+
+      for (final place in chunk) {
+        final documentId = '${place['contentId'] ?? place['contentid'] ?? ''}'
+            .trim();
+        if (documentId.isEmpty) {
+          continue;
+        }
+
+        final docRef = _firestore
+            .collection(tourPlacesCollection)
+            .doc(documentId);
+        batch.set(docRef, {
+          ...place,
+          'updatedAt': FieldValue.serverTimestamp(),
+        }, SetOptions(merge: true));
+        savedCount++;
+      }
+
+      await batch.commit();
+    }
+
+    return savedCount;
+  }
+}
